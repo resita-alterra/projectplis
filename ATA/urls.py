aff1d103976ed599facdaef61150acbf8cba2798
resita-alterra <resita@alterra.id>
2019-07-26 01:04:07 +0700

    ...

--- ATA/urls.py
+++ ATA/urls.py
@@ -1,8 +1,7 @@
-
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('ATA.urls'))
+    path('search/', views.search, name="search"),
 ]
