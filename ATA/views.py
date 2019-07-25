9adb1ec8230882303f3db0a8bb5ad34e15cc9302
resita-alterra <resita@alterra.id>
2019-07-26 01:25:34 +0700

    search engine

--- ATA/views.py
+++ ATA/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import VocabsIndo, Artikel
 
 # Create your views here.
+def search(request):
+    if request.method == 'GET':
+        ini = request.GET.get('cari')
+        if ini is not None:
+            try:
+                arti = VocabsIndo.objects.get(indo=ini)
+                return render(request, 'searchkata.html', {'kata':ini,'arti': arti})
+            except:
+                return render(request,'searchkata.html')
+
+            
+    return render(request,'search.html')
